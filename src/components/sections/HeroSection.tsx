997332fa7b2c1f16dc7106be52ca749e6d6211eb
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-07-03 15:16:58 +0600

    feat: update HeroSection to use buttons for navigation and improve styling

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,8 +1,12 @@
+import { Plus } from 'lucide-react';
+import { Button } from '../ui/button';
+import { Link } from 'react-router';
+
 export default function HeroSection() {
   return (
     <section className="max-w-7xl mx-auto py-8 px-5 lg:px-0">
-      <div className="bg-green-500 py-20 flex flex-col items-center text-center rounded-xl">
-        <h1 className="text-4xl md:text-5xl font-bold text-white font-['Roboto'] mb-4">
+      <div className="bg-green-500 py-10 lg:py-20 px-5 flex flex-col items-center text-center rounded-2xl">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4">
           The Ultimate Solution for your Library Management
         </h1>
         <p className="text-lg md:text-xl text-green-100 font-['Roboto'] mb-8 max-w-xl">
@@ -10,12 +14,14 @@ export default function HeroSection() {
           community with ease.
         </p>
         <div className="flex gap-4">
-          <button className="bg-green-400 hover:bg-green-300 text-white font-['Roboto'] font-semibold py-3 px-6 rounded shadow transition">
-            Get Started
-          </button>
-          <button className="bg-white hover:bg-green-100 text-green-500 font-['Roboto'] font-semibold py-3 px-6 rounded shadow transition border border-green-400">
-            Learn More
-          </button>
+          <Link to="/books">
+            <Button variant="secondary">All Books</Button>
+          </Link>
+          <Link to="/create-book">
+            <Button variant="secondary">
+              <Plus /> Add New
+            </Button>
+          </Link>
         </div>
       </div>
     </section>
